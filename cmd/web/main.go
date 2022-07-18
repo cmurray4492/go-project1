b5c4fd56e200479a34cc0b550c2cae57ea58d91f
cmurray4492 <35826709+cmurray4492@users.noreply.github.com>
2022-07-17 22:18:51 -0500

    added sessions

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
+	"github.com/alexedwards/scs/v2"
 	"github.com/cmurray4492/go-project1/pkg/config"
 	"github.com/cmurray4492/go-project1/pkg/handlers"
 	"github.com/cmurray4492/go-project1/pkg/render"
@@ -12,9 +14,21 @@ import (
 
 const portNumber = ":8080"
 
+var app config.AppConfig
+var session *scs.SessionManager
+
 // main is the main function
 func main() {
-	var app config.AppConfig
+
+	app.InProduction = false
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
 
 	tc, err := render.CreateTemplateCache()
 	if err != nil {
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -14,6 +14,9 @@ func routes(app *config.AppConfig) http.Handler {
 
 	// Middleware
 	mux.Use(middleware.Recoverer)
+	// mux.Use(WriteToConsole)
+	mux.Use(NoSurf)
+	mux.Use(SessionLoad)
 
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
